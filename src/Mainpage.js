ad647b8a5598c075a2e64a0dcb0d1475204c198f
balkary <yoonwoorin13@knu.ac.kr>
2025-05-27 20:08:33 +0900

    endpoint 수정

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -28,7 +28,7 @@ function Mainpage() {
     setIsGenerating(true);
     try {
     // 백엔드로 URL 전달 (POST 요청)
-    const response = await fetch('/api/url', {
+    const response = await fetch('/api/receive-url', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
